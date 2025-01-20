580f42d4bbb487dc57ae6be2c9bfb33f2563c9aa
David Bissaco da Silva <davidbissacodasilva@gmail.com>
2025-01-20 20:34:33 -0300

    inserindo a seed dos games

--- src/main/java/com/david/dslist/entities/Game.java
+++ src/main/java/com/david/dslist/entities/Game.java
@@ -24,25 +24,29 @@ public class Game {
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 
 	public Game() {
 
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
-			String shortDescription, String longDescription) {
-		this.id = id;
-		this.title = title;
-		this.year = year;
-		this.genre = genre;
-		this.platforms = platforms;
-		this.score = score;
-		this.imgUrl = imgUrl;
-		this.shortDescription = shortDescription;
-		this.longDescription = longDescription;
-	}
+	public Game(String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+            String shortDescription, String longDescription) {
+    this.title = title;
+    this.year = year;
+    this.genre = genre;
+    this.platforms = platforms;
+    this.score = score;
+    this.imgUrl = imgUrl;
+    this.shortDescription = shortDescription;
+    this.longDescription = longDescription;
+}
+
 
 	public Long getId() {
 		return id;
