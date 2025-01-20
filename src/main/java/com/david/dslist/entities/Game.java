e2f7c9d3bf131fd8622bfc974a9a0989c8b5069b
David Bissaco da Silva <davidbissacodasilva@gmail.com>
2025-01-20 20:00:57 -0300

    corrigindo e melhorando o commit anterior

--- src/main/java/com/david/dslist/entities/Game.java
+++ src/main/java/com/david/dslist/entities/Game.java
@@ -21,7 +21,8 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
 	private String shortDescription;
 	private String longDescription;
@@ -30,13 +31,14 @@ public class Game {
 
 	}
 
-	public Game(Long id, String title, String year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -58,11 +60,11 @@ public class Game {
 		this.title = title;
 	}
 
-	public String getYear() {
+	public Integer getYear() {
 		return year;
 	}
 
-	public void setYear(String year) {
+	public void setYear(Integer year) {
 		this.year = year;
 	}
 
@@ -74,12 +76,20 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+	
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
